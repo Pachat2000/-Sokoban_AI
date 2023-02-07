2b645f6a111a67524248c80083e08d99e6f5621f
Cécile CHEN <cecilechen14@gmail.com>
2023-02-07 18:08:05 +0100

    prototypes

--- src_cpp/mysok.h
+++ src_cpp/mysok.h
@@ -27,132 +27,163 @@
 #define MAN2_ON_TARGET 9
 #define END_OF_LINE 10
 
-char board_str[] = {' ','_','.','#','$','*','1','u','2','d','a'};
+using namespace std;
+
+char board_str[] = {' ', '_', '.', '#', '$', '*', '1', 'u', '2', 'd', 'a'};
 std::string move_str[] = {"Up", "Down", "Left", "Right", "Wait"};
 
-struct sok_board_t {  
+typedef struct position {
+  int x;
+  int y;
+} Position;
+
+// PROTOTYPES
+int** move(vector<string> list_moves, int** tab_init);
+vector<string> IDD (int Max_Depth, int ** Table,  position * List_impossible_moves);
+vector<Position> get_impossible_positions (int ** board);
+bool check_corner(int** Table, Position pos);
+bool check_wall_line(Position forbid1, Position forbid2, int** tab_init);
+
+struct sok_board_t {
   int board[NBL][NBC];
   int board_nbl;
   int man1_x;
   int man1_y;
   int man2_x;
   int man2_y;
-  
+
   sok_board_t();
   void print_board();
-  void load(char* _file);
+  void load(char *_file);
   void move_option();
   bool verife_win();
 };
 
+
 inline sok_board_t::sok_board_t() {
-  for(int i = 0; i < NBL; i++)
-    for(int j = 0; j < NBC; j++)
+  for (int i = 0; i < NBL; i++)
+    for (int j = 0; j < NBC; j++)
       board[i][j] = FREE;
 }
 
 inline void sok_board_t::print_board() {
-  for(int i = 0; i < board_nbl; i++) {
-    for(int j = 0; j < NBC; j++) {
-      if(board[i][j]==END_OF_LINE) break;
+  for (int i = 0; i < board_nbl; i++) {
+    for (int j = 0; j < NBC; j++) {
+      if (board[i][j] == END_OF_LINE)
+        break;
       printf("%c", board_str[board[i][j]]);
     }
     printf("\n");
   }
 }
 
-inline bool sok_board_t::verife_win(){
-  for(int i = 0; i < board_nbl; i++) {
-    for(int j = 0; j < NBC; j++) {
-      if(board_str[board[i][j]]=='.' || board_str[board[i][j]]=='u')
-      return false;}}
-      return true;
+
+// TO-DO pour Bruno: Il faudrait que la fonction verif win prenne
+// en argument la tableau initial et la liste de chemin pour pouvoir
+// l'utiliser dans une fonction move que Manal va faire (plus pratique pour pachat)
+inline bool sok_board_t::verife_win() {
+  for (int i = 0; i < board_nbl; i++) {
+    for (int j = 0; j < NBC; j++) {
+      if (board_str[board[i][j]] == '.' || board_str[board[i][j]] == 'u')
+        return false;
+    }
+  }
+  return true;
 }
 
-inline void sok_board_t::move_option(){
-  int posx=0;
-  int posy=0;
+inline void sok_board_t::move_option() {
+  int posx = 0;
+  int posy = 0;
   int option[4][2];
-  for(int i = 0; i < 4; i++) {
-    for(int j = 0; j < 2; j++) {
-      option[i][j]=-1;}}
-   for(int i = 0; i < board_nbl; i++) {
-    for(int j = 0; j < NBC; j++) {
-      if (board_str[board[i][j]]=='1'){
-        posx=j;
-        posy=i;}
-    }}
-    int index=0;
-    int x=0;
-    int y=0;
-    for(int i = 0; i < 4; i++) {
-      if(i<2){
-        x=posx-1+i*2;
-        y=posy;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 2; j++) {
+      option[i][j] = -1;
+    }
+  }
+  for (int i = 0; i < board_nbl; i++) {
+    for (int j = 0; j < NBC; j++) {
+      if (board_str[board[i][j]] == '1') {
+        posx = j;
+        posy = i;
       }
-      else{
-        x=posx;
-        y=posy-1+(i-2)*2;}
-      if(board_str[board[y][x]]=='$'){
-          if(board_str[board[(posy+(posy-y)*-2)][(posx+(posx-x)*-2)]]!='$' && board_str[board[(posy+(posy-y)*-2)][(posx+(posx-x)*-2)]]!='#'){
-            option[index][0]=x;
-            option[index][1]=y;
-        }}
-        else if(board_str[board[y][x]]!='#'){
-          option[index][0]=x;
-          option[index][1]=y;
-          index++;
-        }
+    }
+  }
+  int index = 0;
+  int x = 0;
+  int y = 0;
+  for (int i = 0; i < 4; i++) {
+    if (i < 2) {
+      x = posx - 1 + i * 2;
+      y = posy;
+    } else {
+      x = posx;
+      y = posy - 1 + (i - 2) * 2;
+    }
+    if (board_str[board[y][x]] == '$') {
+      if (board_str[board[(posy + (posy - y) * -2)]
+                         [(posx + (posx - x) * -2)]] != '$' &&
+          board_str[board[(posy + (posy - y) * -2)]
+                         [(posx + (posx - x) * -2)]] != '#') {
+        option[index][0] = x;
+        option[index][1] = y;
       }
-      for(int i = 0; i < 4; i++) {
-      if(option[i][0]!=-1)
-      printf("next postion=%d;%d\n",option[i][0],option[i][1]);}
-  
+    } else if (board_str[board[y][x]] != '#') {
+      option[index][0] = x;
+      option[index][1] = y;
+      index++;
+    }
+  }
+  for (int i = 0; i < 4; i++) {
+    if (option[i][0] != -1)
+      printf("next postion=%d;%d\n", option[i][0], option[i][1]);
+  }
 }
-inline void sok_board_t::load(char* _file) {
+
+inline void sok_board_t::load(char *_file) {
   FILE *fp = fopen(_file, "r");
   char *line = NULL;
   size_t len = 0;
   ssize_t nread;
   if (fp == NULL) {
-    perror("fopen"); exit(EXIT_FAILURE);
+    perror("fopen");
+    exit(EXIT_FAILURE);
   }
   board_nbl = 0;
   while ((nread = getline(&line, &len, fp)) != -1) {
-    if(((int)nread) > 0) {
+    if (((int)nread) > 0) {
       bool read_ok = false;
-      for(int i = 0; i < nread; i++) {
-        if(line[i] == board_str[FREE]) {
+      for (int i = 0; i < nread; i++) {
+        if (line[i] == board_str[FREE]) {
           board[board_nbl][i] = FREE;
-        } else if(line[i] == board_str[TARGET]) {
+        } else if (line[i] == board_str[TARGET]) {
           board[board_nbl][i] = TARGET;
-        } else if(line[i] == board_str[WALL]) {
+        } else if (line[i] == board_str[WALL]) {
           read_ok = true;
           board[board_nbl][i] = WALL;
-        } else if(line[i] == board_str[CRATE_ON_FREE]) {
+        } else if (line[i] == board_str[CRATE_ON_FREE]) {
           board[board_nbl][i] = CRATE_ON_FREE;
-        } else if(line[i] == board_str[CRATE_ON_TARGET]) {
+        } else if (line[i] == board_str[CRATE_ON_TARGET]) {
           board[board_nbl][i] = CRATE_ON_TARGET;
-        } else if(line[i] == board_str[MAN1_ON_FREE]) {
+        } else if (line[i] == board_str[MAN1_ON_FREE]) {
           man1_x = board_nbl;
-          man1_y = i; 
+          man1_y = i;
           board[board_nbl][i] = MAN1_ON_FREE;
-        } else if(line[i] == board_str[MAN1_ON_TARGET]) {
+        } else if (line[i] == board_str[MAN1_ON_TARGET]) {
           man1_x = board_nbl;
-          man1_y = i; 
+          man1_y = i;
           board[board_nbl][i] = MAN1_ON_TARGET;
-        } else if(line[i] == board_str[MAN2_ON_FREE]) {
+        } else if (line[i] == board_str[MAN2_ON_FREE]) {
           man2_x = board_nbl;
-          man2_y = i; 
+          man2_y = i;
           board[board_nbl][i] = MAN2_ON_FREE;
-        } else if(line[i] == board_str[MAN2_ON_TARGET]) {
+        } else if (line[i] == board_str[MAN2_ON_TARGET]) {
           man2_x = board_nbl;
-          man2_y = i; 
+          man2_y = i;
           board[board_nbl][i] = MAN2_ON_TARGET;
         }
       }
-      if(read_ok) {
-        board[board_nbl][nread-1]=END_OF_LINE;
+      if (read_ok) {
+        board[board_nbl][nread - 1] = END_OF_LINE;
         board_nbl++;
       }
     }
--- src_cpp/r0.cpp
+++ src_cpp/r0.cpp
@@ -1,15 +1,37 @@
-#include <cstdio>
+//#include <cstdio>
+#include "mysok.h"
 #include <cstdlib>
 #include <string.h>
 #include <string>
-#include "mysok.h"
+#include <vector>
 
-int main(int _ac, char** _av) {
-  if(_ac != 2) { printf("usage: %s SOK_FILE\n", _av[0]); return 0;}
+using namespace std;
+
+//creation liste des positions impossibles
+vector<Position> get_impossible_positions (int ** board) {
+    vector<Position> forbid_pos;
+
+    return forbid_pos;
+}
+
+// pour une case (pos) regarde si il y a 2 murs adjacent et qu'il n'y pas de point final 
+bool check_corner(char* Table, Position pos) {
+    return true;
+}
+// regarde il y as un mur complet entre 2 positions interdite et qu'il n'y a pas de point final 
+bool check_wall_line(Position pos1, Position pos2, char * Table) {
+    return true;
+}
+
+int main(int _ac, char **_av) {
+  if (_ac != 2) {
+    printf("usage: %s SOK_FILE\n", _av[0]);
+    return 0;
+  }
   sok_board_t S;
   S.load(_av[1]);
   S.print_board();
-  if(S.verife_win())
+  if (S.verife_win())
     printf("win\n");
   return 0;
 }
